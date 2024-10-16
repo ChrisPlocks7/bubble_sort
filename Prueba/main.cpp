85795260310bb93d3877c4a7fc0d7759a0478582
Christopher Espinosa <razochris7510@gmail.com>
2024-10-16 17:21:52 -0600

    Initial Commit

---
+++ Prueba/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Prueba
+//
+//  Created by Christopher Espinosa on 16/10/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
